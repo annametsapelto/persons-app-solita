6686d1cbdbdd107b2d0fe4b83a8b0005394f69e0
Anna Metsäpelto <anna.metsapelto@gmail.com>
2020-12-23 19:11:48 +0200

    Add Nameslist.js to Components and to App.js

--- solita-persons-app/src/App.js
+++ solita-persons-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Nameslist from './Components/nameslist';
 
 function App() {
   return (
     <div className="App">
-
+      <Nameslist/>
     </div>
   );
 }
---
+++ solita-persons-app/src/Components/nameslist.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Names from './Data/names.json'
+
+export default Nameslist;
